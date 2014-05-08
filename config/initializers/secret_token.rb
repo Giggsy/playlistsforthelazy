ac93c4bd9c87395948f03cd04dc5d1df4eaf41d0
Averyan Turetsky <Averyanturetsky@gmail.com>
2014-05-07 21:12:03 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playlistsftl::Application.config.secret_key_base = '39d59547167c6952e204ed680e4ad7371419ba60deb8c89e9ecf2b8a2d74af41b790a175ff3adef8a207c65b99d398eed3d092aa76870bf7db1a79af82b61a7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Playlistsftl::Application.config.session_store :cookie_store, key: '_playlistsftl_session'
